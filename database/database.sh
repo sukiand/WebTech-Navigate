fa0ad32e4a5ca74e7796bb561781608572cf5b36
Changyun Gong <cgong4@jhu.edu>
2015-09-16 01:01:32 -0700

    new database

--- database/database.sh
+++ database/database.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-mongoimport --db navigate --collection websites --drop --file websites.json 
+mongoimport --db navigate --collection websites --drop --file websites.json ;
+echo "websites import complete";
+
+mongoimport --db navigate --collection QAs --drop --file QAs.json ;
+echo "QAs import complete";
+
+mongoimport --db navigate --collection profiles --drop --file profiles.json ;
+echo "profiles import complete";
