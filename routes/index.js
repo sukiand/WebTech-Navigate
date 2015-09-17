c1ae8d5e28cd4c5c98fc96b4ee74ee945445fc5e
Ubuntu <ubuntu@ip-172-31-32-227.us-west-2.compute.internal>
2015-09-17 02:12:08 +0000

    from aws ec2 v1.0

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,18 @@ router.get('/index', function(req, res, next) {
 });
 
 router.get('/', function(req, res, next) {
-  res.render('common/menubar/index', { title: 'Express' });
+  res.render('common/menubar/index', { title: 'Web Hotpot' });
 });
 
 
 router.get('/about', function(req, res, next) {
-    res.render('common/menubar/index', { title: 'Express' });
+    res.render('common/menubar/index', { title: 'Web Hotpot' });
 });
 
 router.get('/components/:mod/:page', function(req, res, next) {
   var mod = req.params.mod;
   var page = req.params.page;
-  res.render('components/' + mod + '/' + page, { title: 'Express' });
+  res.render('components/' + mod + '/' + page, { title: 'Web Hotpot' });
 });
 
 // router.get('/angular', function(req, res, next) {
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+nohup npm start > ~/log/log.txt &
