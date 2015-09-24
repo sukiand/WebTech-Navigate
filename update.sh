2d1bfd7ff1c11a9bcd57552088c96fd72b5626e8
Changyun Gong <cgong4@jhu.edu>
2015-09-24 05:22:33 -0700

    update.sh

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ git fetch --all
 
 git reset --hard origin/master
 
-echo "servers: change No. of this server\n"
+echo "servers: 1 change No. of this server\n\t\t 2 change the url of the database"
 echo "databses: update the databse please run the script in database folder\n"
