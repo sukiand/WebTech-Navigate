7ca7e5eea10f7841a984d249dfcfc536164b7acc
Changyun Gong <cgong4@jhu.edu>
2015-09-24 05:38:10 -0700

    improve scripts

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+netstat -anl | grep 300
+
+killall node
+
+netstat -anl | grep 300
+
+echo "restart"
 nohup npm start > ~/log/log.txt &
--- update.sh
+++ update.sh
@@ -4,5 +4,10 @@ git fetch --all
 
 git reset --hard origin/master
 
-echo "servers: 1 change No. of this server\n\t\t 2 change the url of the database"
-echo "databses: update the databse please run the script in database folder\n"
+npm install
+
+bower install
+
+echo "servers: \t1 change No. of this server\n\t\t 2 change the url of the database"
+
+echo "databses: \tupdate the databse please run the script in database folder\n"
